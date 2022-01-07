a9270cadd933d960f0ddabe4c572ca9a792ea945
Braante <brandow.kane@gmail.com>
2022-01-07 12:10:53 +0100

    html css 1

--- serv/main.go
+++ serv/main.go
@@ -39,13 +39,10 @@ func main() {
 		rep := hangmanweb.PrintTable(b)
 		data := TodoPageData{
 			PageTitle: rep,
-			Todos: []Todo{
-				{Title: "Task 1", Done: false},
-				{Title: "Task 2", Done: true},
-				{Title: "Task 3", Done: true},
-			},
 		}
 		tmpl.Execute(w, data)
 	})
+	fs := http.FileServer(http.Dir("css/"))
+	http.Handle("/css/", http.StripPrefix("/css/", fs))
 	http.ListenAndServe(":8080", nil)
 }
