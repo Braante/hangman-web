8beafafdbf145f18b8b466b5f370bd8aa64ef498
Hbourmaud <hugo.bourmaud@ynov.com>
2022-01-20 15:46:06 +0100

    presquetout

--- serv/main.go
+++ serv/main.go
@@ -1,30 +1,37 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"hangmanweb"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"text/template"
 )
 
 type WinData struct {
-	PageWin string
+	WordFind    string
+	Scoreboards string
+}
+
+type LoseData struct {
+	WordFind    string
+	Scoreboards string
 }
 
 type SaveStruct struct {
-	Score    int
 	NameSave string
+	Score    int
 }
 
 type TodoPageData struct {
 	PageTitle    string
 	Attemptsleft int
-	TextDeco     string
+	LWU          string
 	LetterUsed   string
 	UserName     string
+	Restart      string
 }
 
 type StartData struct {
@@ -37,88 +44,132 @@ func main() {
 		dota := StartData{}
 		tmpl1.Execute(w, dota)
 	})
-	tmplWin := template.Must(template.ParseFiles("html/win.html"))
-	http.HandleFunc("/win", func(w http.ResponseWriter, r *http.Request) {
-		doti := WinData{}
-		tmplWin.Execute(w, doti)
-	})
-	TextDeco := ""
+	chr := ""
+	lose := false
+	usersaved := false
+	var rep string
+	win := false
 	var a []byte
 	var b []byte
 	attempts := 10
-	var min bool
-	var maj bool
+	min := false
+	maj := false
 	var tableauX []byte
 	name := ""
+	var listwords string
+	score := 0
 	tmpl := template.Must(template.ParseFiles("html/index.html"))
 	http.HandleFunc("/hangman", func(w http.ResponseWriter, r *http.Request) {
-		if TextDeco != "You win" && TextDeco != "You lose" {
+		switch r.Method {
+		case "GET":
+			http.Redirect(w, r, "/", 301)
+		case "POST":
+			fmt.Println("namia:", name)
+			if name == "" {
+				name = r.FormValue("Username")
+			}
+			fmt.Println("hfeu:", win)
+			if !win && listwords == "" {
+				fmt.Println("aieaieaie")
+				listword := r.FormValue("Difficulty")
+				listwords = listword
+			}
+			letter := r.FormValue("letter")
+			fmt.Println("listorsd:", listwords)
+			if (!min && !maj) || win || lose {
+				fmt.Println("peteuurtur")
+				a = hangmanweb.WordChoose(listwords)
+				/* --- cheat code --- */
+				fmt.Println(string((a)))
+				b = hangmanweb.PlusALea(a)
+				min, maj = hangmanweb.Initialisation(a)
+				win = false
+				lose = false
+			}
 
-			switch r.Method {
-			case "GET":
-				http.Redirect(w, r, "/", 301)
-
-			case "POST":
-				if name == "" {
-					name = r.FormValue("Username")
-				}
-				listwords := r.FormValue("Difficulty")
-				letter := r.FormValue("letter")
-				if len(listwords) != 0 {
-					a = hangmanweb.WordChoose(listwords)
-					/* --- cheat code --- */
-					fmt.Println(string((a)))
-					b = hangmanweb.PlusALea(a)
-					min, maj = hangmanweb.Initialisation(a)
-					//fmt.Print(min, maj)
-				}
-
-				attempts, tableauX = hangmanweb.CheckAccents(min, maj, b, a, attempts, letter, tableauX)
-				if attempts == 11 {
-					saveos, _ := os.Open("save.txt")
-					info, _ := os.Stat("save.txt")
-					size := info.Size()
-					old := make([]byte, size)
-					saveos.Read(old)
-					saveos.Close()
-					m := SaveStruct{attempts, name}
-					saved, _ := json.Marshal(m)
-					for i := 0; i < len(old); i++ {
-						saved = append(saved, old[i])
-					}
-					ioutil.WriteFile("save.txt", saved, 0777)
-					b = a
-					http.Redirect(w, r, "/win", 301)
-					TextDeco = "You win"
-				}
-				if attempts <= 0 {
-					TextDeco = "You lose"
-				}
-				//fmt.Println(attempts)
-				//fmt.Print(letter)
+			attempts, tableauX = hangmanweb.CheckAccents(min, maj, b, a, attempts, letter, tableauX)
+			if attempts == 11 {
+				b = a
+				attempts = 10
+				rep = ""
+				win = true
+				usersaved = false
+				http.Redirect(w, r, "/win", 301)
+			}
+			if attempts <= 0 {
+				attempts = 10
+				rep = ""
+				lose = true
+				score = 0
+				http.Redirect(w, r, "/lose", 301)
 			}
 		}
-		rep := hangmanweb.PrintTable(b)
+		rep = hangmanweb.PrintTable(b)
 		if attempts == 11 || attempts <= 0 {
 			data := TodoPageData{
 				PageTitle:    rep,
-				TextDeco:     TextDeco,
+				LWU:          listwords,
 				Attemptsleft: attempts,
 				UserName:     name,
 			}
 			tmpl.Execute(w, data)
 		} else {
 			letterused := hangmanweb.PrintTableEspace(tableauX)
+			fmt.Println("passagelaici")
 			data := TodoPageData{
 				PageTitle:    rep,
 				Attemptsleft: attempts,
 				LetterUsed:   letterused,
 				UserName:     name,
+				LWU:          listwords,
 			}
 			tmpl.Execute(w, data)
 		}
-
 	})
+
+	tmplWin := template.Must(template.ParseFiles("html/win.html"))
+	http.HandleFunc("/win", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			if !win {
+				http.Redirect(w, r, "/", 301)
+			}
+		case "POST":
+			if !usersaved && win {
+				oldchr, _ := ioutil.ReadFile("save.txt")
+				oldchrp := hangmanweb.PrintTable(oldchr)
+				score++
+				file, _ := os.Create("save.txt")
+				scorestr := strconv.FormatInt(int64(score), 10)
+				chr = name + " " + scorestr + "<br>" + oldchrp
+				file.WriteString(chr)
+				usersaved = true
+				file.Close()
+			}
+		}
+		fmt.Println("scoreboardfjier:0", chr)
+		doti := WinData{
+			WordFind:    rep,
+			Scoreboards: chr,
+		}
+		tmplWin.Execute(w, doti)
+	})
+
+	tmplLose := template.Must(template.ParseFiles("html/lose.html"))
+	http.HandleFunc("/lose", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			if !lose {
+				http.Redirect(w, r, "/", 301)
+			}
+		}
+		resp := hangmanweb.PrintTable(a)
+		dyti := LoseData{
+			WordFind: resp,
+		}
+		tmplLose.Execute(w, dyti)
+	})
+
 	fs := http.FileServer(http.Dir("css/"))
 	http.Handle("/css/", http.StripPrefix("/css/", fs))
 	fs2 := http.FileServer(http.Dir("images/"))
